e41a2e2fbe411935d9a8513d1bc97656d26df3c7
gonenshafry <gonenshafry@hotmail.com>
2022-09-09 11:04:37 +0300

    Build to TravisCI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          refreshed
+          TravisCI build test
         </p>
         <a
           className="App-link"
